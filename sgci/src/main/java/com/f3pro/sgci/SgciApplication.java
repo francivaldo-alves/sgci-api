d5b7154cb8ca6f1dd91c289a445e38fb974e574d
Francivaldo Alves <francivaldo.alves@gmail.com>
2024-10-27 18:35:46 -0300

    feat: inclusão código fonte inicial

---
+++ sgci/src/main/java/com/f3pro/sgci/SgciApplication.java
@@ -0,0 +1,13 @@
+package com.f3pro.sgci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgciApplication.class, args);
+	}
+
+}
---
+++ sgci/src/test/java/com/f3pro/sgci/SgciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3pro.sgci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
