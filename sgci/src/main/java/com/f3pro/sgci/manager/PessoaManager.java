9873ac8eff21d21f645808c1f3a1626c24c23045
Francivaldo Alves <francivaldo.alves@gmail.com>
2024-10-30 22:44:56 -0300

    refactor: melhorando a legibilidade da class PessoaManager

--- sgci/src/main/java/com/f3pro/sgci/manager/PessoaManager.java
+++ sgci/src/main/java/com/f3pro/sgci/manager/PessoaManager.java
@@ -4,10 +4,11 @@ import com.f3pro.sgci.model.Endereco;
 import com.f3pro.sgci.model.Pessoa;
 import com.f3pro.sgci.repositories.PessoaRespository;
 import com.f3pro.sgci.schema.*;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.mapstruct.Mapper;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,33 +19,68 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper
 @Service
 @Validated
 public class PessoaManager {
+
     @Autowired
     private PessoaRespository pessoaRespository;
 
     @Transactional
     public Pessoa createPessoa(PessoaReq req) {
-        Endereco endereco = new Endereco(req.endereco().cep(), req.endereco().estado(), req.endereco().cidade(),
-                req.endereco().rua(), req.endereco().bairro(), req.endereco().numero());
-        Pessoa pessoa = new Pessoa(endereco, req.nome(), req.tipo(), req.documento(), req.profissao(),
-                req.estadoCivil());
+        Endereco endereco = criarEndereco(req.getEndereco());
+        Pessoa pessoa = new Pessoa(endereco, req.getNome(), req.getTipo(), req.getDocumento(), req.getProfissao(), req.getEstadoCivil());
         return pessoaRespository.save(pessoa);
     }
 
     @Transactional
     public void deletePessoa(Long idPessoa) {
-        Pessoa pessoa = pessoaRespository.findById(idPessoa).orElseThrow();
+        Pessoa pessoa = pessoaRespository.findById(idPessoa)
+                .orElseThrow(() -> new EntityNotFoundException("Pessoa não encontrada com ID: " + idPessoa));
         pessoaRespository.delete(pessoa);
     }
 
     public ResponsePagedCommom<PessoaResponse> findAll(@Valid PessoaFilter filtros) {
-        List<PessoaResponse> listResponse = new ArrayList<PessoaResponse>();
+        Specification<Pessoa> filtrosCustomizados = criarEspecificacao(filtros);
+        Page<Pessoa> listPessoaDB = pessoaRespository.findAll(filtrosCustomizados,
+                PageRequest.of(filtros.getPage(), filtros.getSize(), Sort.by(filtros.getDirection(), filtros.getOrdenarPo())));
 
-        Specification<Pessoa> filtrosCustomizados = (root, query, cb) -> {
+        List<PessoaResponse> listResponse = listPessoaDB.stream()
+                .map(this::converterParaPessoaResponse)
+                .collect(Collectors.toList());
+
+        return new ResponsePagedCommom<>(listResponse, listPessoaDB.getTotalElements(), listPessoaDB.getTotalPages(), filtros.getSize());
+    }
+
+    @Transactional
+    public Pessoa updatePessoa(@Valid Long idPessoa, PessoaUpd upd) {
+        Pessoa pessoa = pessoaRespository.findById(idPessoa)
+                .orElseThrow(() -> new EntityNotFoundException("Pessoa não encontrada com ID: " + idPessoa));
+
+        pessoa.setDocumento(upd.documento());
+        pessoa.setEstadoCivil(upd.estadoCivil());
+        pessoa.setNome(upd.nome());
+        pessoa.setProfissao(upd.profissao());
+        pessoa.setTipo(upd.tipo());
+
+        atualizarEndereco(pessoa.getEndereco(), upd.endereco());
+
+        return pessoaRespository.save(pessoa);
+    }
+
+    public PessoaResponse findById(Long idPessoa) {
+        Pessoa pessoa = pessoaRespository.findById(idPessoa)
+                .orElseThrow(() -> new EntityNotFoundException("Pessoa não encontrada com ID: " + idPessoa));
+
+        return converterParaPessoaResponse(pessoa);
+    }
+
+    // Métodos Auxiliares
+
+    private Specification<Pessoa> criarEspecificacao(PessoaFilter filtros) {
+        return (root, query, cb) -> {
             List<Predicate> condicoes = new ArrayList<>();
             if (filtros.getNome() != null) {
                 condicoes.add(cb.like(root.get("nome"), "%" + filtros.getNome() + "%"));
@@ -64,47 +100,28 @@ public class PessoaManager {
             if (filtros.getDocumento() != null) {
                 condicoes.add(cb.equal(root.get("documento"), filtros.getDocumento()));
             }
-
-            return cb.and(condicoes.toArray(Predicate[]::new));
+            return cb.and(condicoes.toArray(new Predicate[0]));
         };
-
-        Page<Pessoa> listPessoaDB = pessoaRespository.findAll(filtrosCustomizados, PageRequest.of(filtros.getPage(), filtros.getSize(), Sort.by(filtros.getDirection(), filtros.getOrdenarPo())));
-        listPessoaDB.forEach(item -> {
-            EnderecoResponse enderecoResponse = EnderecoMapper.INSTANCE.toEnderecoResponse(item.getEndereco());
-            PessoaResponse pessoaResponse = PessoaMapper.INSTANCE.toPessoaResponse(item, enderecoResponse);
-
-            listResponse.add(pessoaResponse);
-
-
-        });
-
-        return new ResponsePagedCommom<PessoaResponse>(listResponse, listPessoaDB.getTotalElements(), listPessoaDB.getTotalPages(), filtros.getSize());
     }
 
-
-    @Transactional
-    public Pessoa updatePessoa(@Valid Long idPessoa, PessoaUpd upd) {
-        Pessoa pessoa = pessoaRespository.findById(idPessoa).orElseThrow();
-        pessoa.setDocumento(upd.documento());
-        pessoa.setEstadoCivil(upd.estadoCivil());
-        pessoa.setNome(upd.nome());
-        pessoa.setProfissao(upd.profissao());
-        pessoa.setTipo(upd.tipo());
-
-        pessoa.getEndereco().setCep(upd.endereco().cep());
-        pessoa.getEndereco().setEstado(upd.endereco().estado());
-        pessoa.getEndereco().setCidade(upd.endereco().estado());
-        pessoa.getEndereco().setRua(upd.endereco().rua());
-        pessoa.getEndereco().setBairro(upd.endereco().bairro());
-        pessoa.getEndereco().setNumero(upd.endereco().numero());
-        return pessoaRespository.save(pessoa);
+    private Endereco criarEndereco(EnderecoReq enderecoReq) {
+        return new Endereco(
+                enderecoReq.cep(), enderecoReq.estado(), enderecoReq.cidade(),
+                enderecoReq.rua(), enderecoReq.bairro(), enderecoReq.numero()
+        );
     }
 
-    public PessoaResponse findById(Long idPessoa) {
-        Pessoa pessoa = pessoaRespository.findById(idPessoa).orElseThrow();
+    private void atualizarEndereco(Endereco endereco, @NotNull EnderecoReq enderecoUpd) {
+        endereco.setCep(enderecoUpd.cep());
+        endereco.setEstado(enderecoUpd.estado());
+        endereco.setCidade(enderecoUpd.cidade());
+        endereco.setRua(enderecoUpd.rua());
+        endereco.setBairro(enderecoUpd.bairro());
+        endereco.setNumero(enderecoUpd.numero());
+    }
+
+    private PessoaResponse converterParaPessoaResponse(Pessoa pessoa) {
         EnderecoResponse enderecoResponse = EnderecoMapper.INSTANCE.toEnderecoResponse(pessoa.getEndereco());
         return PessoaMapper.INSTANCE.toPessoaResponse(pessoa, enderecoResponse);
-
-
     }
 }
--- sgci/src/main/java/com/f3pro/sgci/schema/PessoaReq.java
+++ sgci/src/main/java/com/f3pro/sgci/schema/PessoaReq.java
@@ -4,25 +4,82 @@ import com.f3pro.sgci.model.EstadoCivilEnum;
 import com.f3pro.sgci.model.TipoPessoaEnum;
 import jakarta.validation.constraints.NotNull;
 
-public record PessoaReq(
+public class PessoaReq {
 
         @NotNull
-        String nome,
+        private String nome;
 
         @NotNull
-        EnderecoReq endereco,
+        private EnderecoReq endereco;
 
         @NotNull
-        TipoPessoaEnum tipo,
+        private TipoPessoaEnum tipo;
 
         @NotNull
-        String documento,
+        private String documento;
 
         @NotNull
-        String profissao,
+        private String profissao;
 
         @NotNull
-        EstadoCivilEnum estadoCivil
-) {
+        private EstadoCivilEnum estadoCivil;
 
+        // Construtor
+        public PessoaReq(String nome, EnderecoReq endereco, TipoPessoaEnum tipo, String documento, String profissao, EstadoCivilEnum estadoCivil) {
+                this.nome = nome;
+                this.endereco = endereco;
+                this.tipo = tipo;
+                this.documento = documento;
+                this.profissao = profissao;
+                this.estadoCivil = estadoCivil;
+        }
+
+        // Getters e Setters
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public EnderecoReq getEndereco() {
+                return endereco;
+        }
+
+        public void setEndereco(EnderecoReq endereco) {
+                this.endereco = endereco;
+        }
+
+        public TipoPessoaEnum getTipo() {
+                return tipo;
+        }
+
+        public void setTipo(TipoPessoaEnum tipo) {
+                this.tipo = tipo;
+        }
+
+        public String getDocumento() {
+                return documento;
+        }
+
+        public void setDocumento(String documento) {
+                this.documento = documento;
+        }
+
+        public String getProfissao() {
+                return profissao;
+        }
+
+        public void setProfissao(String profissao) {
+                this.profissao = profissao;
+        }
+
+        public EstadoCivilEnum getEstadoCivil() {
+                return estadoCivil;
+        }
+
+        public void setEstadoCivil(EstadoCivilEnum estadoCivil) {
+                this.estadoCivil = estadoCivil;
+        }
 }
